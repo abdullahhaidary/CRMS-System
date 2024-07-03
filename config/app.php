bfb30238356916a90dac44b4e60f72529435aa2f
unknown <abdullahhadiary4262@gmail.com>
2024-07-03 18:15:27 +0430

    multiple language added

--- config/app.php
+++ config/app.php
@@ -83,7 +83,11 @@ return [
     'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
 
     'faker_locale' => env('APP_FAKER_LOCALE', 'en_US'),
-
+    'available_locals'=>[
+        'English'=>'english',
+        'dari'=>'dari',
+        'pashto'=>'pashto'
+    ],
     /*
     |--------------------------------------------------------------------------
     | Encryption Key
