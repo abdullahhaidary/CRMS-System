d6dcced3f520ca19ee669ec7540fcbd18774deff
abdulwali <abdulwaliaryan897@gmail.com>
2024-07-28 09:57:22 +0430

    test

--- resources/views/profile/profile_detail.blade.php
+++ resources/views/profile/profile_detail.blade.php
@@ -18,17 +18,11 @@ Profile Details
                     {{Auth::user()->name}}
                 </div>
 
-{{--                <div class="nav flex-column">--}}
-{{--                    <a href="#"><i class="bi bi-person-circle"></i> Profile</a>--}}
-{{--                    <a href="#"><i class="bi bi-gear-fill"></i> Settings</a>--}}
-{{--                    <a href="#"><i class="bi bi-bell-fill"></i> Notifications</a>--}}
-{{--                    <a href="{{route('logout')}}"><i class="bi bi-box-arrow-right"></i> Logout</a>--}}
-{{--                </div>--}}
                 <div class="profile_links">
                     <ul>
                         <li><a href="{{route('home')}}"> <i class="fas fa-home" style="color: #0a53be;font-size: 20px"></i> صفحه اصلی </a></li>
                         <li><a href="{{url('change/password/'. \Illuminate\Support\Facades\Auth::user()->id)}}"> <i class="fas fa-key" style="color: #e14439;font-size: 20px"></i>تغیر فاسورد </a></li>
-{{--                        <li><a href="">Friends</a></li>--}}
+                        <li><a href="">Friends</a></li>
                     </ul>
                 </div>
             </div>
@@ -38,7 +32,7 @@ Profile Details
                 @if(session()->has('success'))
                     <div class="alert alert-success" style="background-color: #22ec8f; height: 30px; align-content: center;">
                         {{session('success')}}
-                        {{-- yup --}}
+                         yup
                     </div>
                 @endif
                 <div class="personal_information">
@@ -80,7 +74,7 @@ Profile Details
             <div class="profile-image-dialog py-5" id="profile-edit-dialog">
                 <div class="dialog-content" id="profile-edit-content">
                     <span class="close-btn" onclick="closeProfileDialog()">&times;</span>
-                    {{-- <br><br> --}}
+                     <br><br>
                     <h3 class="text-2xl mb-5">Edit Profile</h3>
                     <div class="">
                         <label for="name" style="text-align:start;">Name: </label>
@@ -121,3 +115,186 @@ Profile Details
         </form>
     </div>
 @endsection
+
+
+
+{{--@extends('profile.template')--}}
+{{--@section('title', 'Profile Details')--}}
+{{--@section('main')--}}
+
+{{--    <!-- Navbar -->--}}
+{{--    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">--}}
+{{--        <a class="navbar-brand" href="#">Profile</a>--}}
+{{--        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">--}}
+{{--            <span class="navbar-toggler-icon"></span>--}}
+{{--        </button>--}}
+{{--        <div class="collapse navbar-collapse" id="navbarNav">--}}
+{{--            <ul class="navbar-nav ml-auto">--}}
+{{--                <li class="nav-item">--}}
+{{--                    <a class="nav-link" href="{{ route('home') }}">Home</a>--}}
+{{--                </li>--}}
+{{--                <li class="nav-item">--}}
+{{--                    <a class="nav-link" href="{{ route('logout') }}">Logout</a>--}}
+{{--                </li>--}}
+{{--            </ul>--}}
+{{--        </div>--}}
+{{--    </nav>--}}
+
+{{--    <div class="container mt-5">--}}
+{{--        <div class="row justify-content-center">--}}
+{{--            <div class="col-md-8">--}}
+{{--                <div class="card">--}}
+{{--                    <div class="card-body text-center">--}}
+{{--                        <div class="position-relative d-inline-block mb-4">--}}
+{{--                            <a href="{{ route('login') }}" class="btn btn-outline-primary position-absolute" style="top: 10px; left: 10px;">--}}
+{{--                                <i class="fas fa-arrow-left"></i>--}}
+{{--                            </a>--}}
+{{--                            <img src="{{ Storage::url('profiles/' . Auth::user()->picture) }}" alt="Profile Picture" class="rounded-circle" width="150" height="150">--}}
+{{--                            <button class="btn btn-outline-secondary position-absolute" style="bottom: 10px; right: 10px;" onclick="openProfileImageDialog()">--}}
+{{--                                <i class="fas fa-edit"></i>--}}
+{{--                            </button>--}}
+{{--                        </div>--}}
+{{--                        <h3>{{ Auth::user()->name }}</h3>--}}
+{{--                        <div class="mt-3">--}}
+{{--                            <a href="{{ route('home') }}" class="btn btn-primary">--}}
+{{--                                <i class="fas fa-home"></i> Home--}}
+{{--                            </a>--}}
+{{--                            <a href="{{ url('change/password/' . Auth::user()->id) }}" class="btn btn-warning">--}}
+{{--                                <i class="fas fa-key"></i> Change Password--}}
+{{--                            </a>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+
+{{--                <!-- Personal Information Section -->--}}
+{{--                <div class="card mt-4">--}}
+{{--                    <div class="card-header">--}}
+{{--                        Personal Information--}}
+{{--                    </div>--}}
+{{--                    <div class="card-body">--}}
+{{--                        @if(session()->has('success'))--}}
+{{--                            <div class="alert alert-success">--}}
+{{--                                {{ session('success') }}--}}
+{{--                            </div>--}}
+{{--                        @endif--}}
+{{--                        <div class="d-flex justify-content-end">--}}
+{{--                            <button class="btn btn-outline-primary" onclick="editForm()">--}}
+{{--                                <i class="fas fa-edit"></i> Edit--}}
+{{--                            </button>--}}
+{{--                        </div>--}}
+{{--                        <div class="personal_information mt-3">--}}
+{{--                            <div class="row">--}}
+{{--                                <div class="col-md-4"><strong>User ID:</strong></div>--}}
+{{--                                <div class="col-md-8">{{ Auth::user()->id }}</div>--}}
+{{--                            </div>--}}
+{{--                            <div class="row">--}}
+{{--                                <div class="col-md-4"><strong>Name:</strong></div>--}}
+{{--                                <div class="col-md-8">{{ Auth::user()->name }}</div>--}}
+{{--                            </div>--}}
+{{--                            <div class="row">--}}
+{{--                                <div class="col-md-4"><strong>Email:</strong></div>--}}
+{{--                                <div class="col-md-8">{{ Auth::user()->email }}</div>--}}
+{{--                            </div>--}}
+{{--                            <div class="row">--}}
+{{--                                <div class="col-md-4"><strong>Type:</strong></div>--}}
+{{--                                <div class="col-md-8">{{ Auth::user()->type }}</div>--}}
+{{--                            </div>--}}
+{{--                            <div class="row">--}}
+{{--                                <div class="col-md-4"><strong>Action:</strong></div>--}}
+{{--                                <div class="col-md-8">{{ Auth::user()->action == 1 ? 'Active' : 'Inactive' }}</div>--}}
+{{--                            </div>--}}
+{{--                            <div class="row">--}}
+{{--                                <div class="col-md-4"><strong>Date Created:</strong></div>--}}
+{{--                                <div class="col-md-8">{{ Auth::user()->created_at ? Auth::user()->created_at->format('Y-m-d') : 'An Error Occurred' }}</div>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+
+{{--        <!-- Edit Profile Form -->--}}
+{{--        <div class="modal fade" id="profileEditModal" tabindex="-1" aria-labelledby="profileEditModalLabel" aria-hidden="true">--}}
+{{--            <div class="modal-dialog">--}}
+{{--                <div class="modal-content">--}}
+{{--                    <form action="{{ route('profile_info_edit') }}" method="POST">--}}
+{{--                        @csrf--}}
+{{--                        <div class="modal-header">--}}
+{{--                            <h5 class="modal-title" id="profileEditModalLabel">Edit Profile</h5>--}}
+{{--                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">--}}
+{{--                                <span aria-hidden="true">&times;</span>--}}
+{{--                            </button>--}}
+{{--                        </div>--}}
+{{--                        <div class="modal-body">--}}
+{{--                            <div class="form-group">--}}
+{{--                                <label for="name">Name:</label>--}}
+{{--                                <input type="text" value="{{ Auth::user()->name }}" class="form-control" name="name" id="name">--}}
+{{--                            </div>--}}
+{{--                            <div class="form-group">--}}
+{{--                                <label for="email">Email:</label>--}}
+{{--                                <input type="email" value="{{ Auth::user()->email }}" class="form-control" name="email" id="email">--}}
+{{--                            </div>--}}
+{{--                            <div class="form-group">--}}
+{{--                                <label for="dob">Date of Birth:</label>--}}
+{{--                                <input type="date" class="form-control" name="dob" id="dob" value="{{ Auth::user()->dob ? Auth::user()->dob->format('Y-m-d') : '' }}">--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                        <div class="modal-footer">--}}
+{{--                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>--}}
+{{--                            <button type="submit" class="btn btn-primary">Save changes</button>--}}
+{{--                        </div>--}}
+{{--                    </form>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+
+{{--        <!-- Change Profile Image Form -->--}}
+{{--        <div class="modal fade" id="profileImageModal" tabindex="-1" aria-labelledby="profileImageModalLabel" aria-hidden="true">--}}
+{{--            <div class="modal-dialog">--}}
+{{--                <div class="modal-content">--}}
+{{--                    <form action="{{ route('profile_change') }}" method="POST" enctype="multipart/form-data">--}}
+{{--                        @csrf--}}
+{{--                        <div class="modal-header">--}}
+{{--                            <h5 class="modal-title" id="profileImageModalLabel">Update Profile Image</h5>--}}
+{{--                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">--}}
+{{--                                <span aria-hidden="true">&times;</span>--}}
+{{--                            </button>--}}
+{{--                        </div>--}}
+{{--                        <div class="modal-body text-center">--}}
+{{--                            <img src="{{ asset('storage/profiles/' . Auth::user()->picture) }}" alt="Profile Image" id="current-profile-image" class="img-thumbnail mb-3">--}}
+{{--                            <input type="file" name="picture" id="profile-image-upload" onchange="previewProfileImage(event)" class="form-control-file">--}}
+{{--                        </div>--}}
+{{--                        <div class="modal-footer">--}}
+{{--                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>--}}
+{{--                            <button type="submit" class="btn btn-primary">Save changes</button>--}}
+{{--                        </div>--}}
+{{--                    </form>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+
+{{--    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>--}}
+{{--    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>--}}
+{{--    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>--}}
+{{--    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/js/all.min.js"></script>--}}
+{{--    <script>--}}
+{{--        function openProfileImageDialog() {--}}
+{{--            $('#profileImageModal').modal('show');--}}
+{{--        }--}}
+
+{{--        function editForm() {--}}
+{{--            $('#profileEditModal').modal('show');--}}
+{{--        }--}}
+
+{{--        function previewProfileImage(event) {--}}
+{{--            var reader = new FileReader();--}}
+{{--            reader.onload = function(){--}}
+{{--                var output = document.getElementById('current-profile-image');--}}
+{{--                output.src = reader.result;--}}
+{{--            }--}}
+{{--            reader.readAsDataURL(event.target.files[0]);--}}
+{{--        }--}}
+{{--    </script>--}}
+
+{{--@endsection--}}
--- resources/views/province_account/account.blade.php
+++ resources/views/province_account/account.blade.php
@@ -1,88 +1,179 @@
-<!DOCTYPE html>
+{{--<!DOCTYPE html>--}}
+{{--<html lang="en">--}}
+{{--<head>--}}
+{{--    <meta charset="UTF-8">--}}
+{{--    <meta name="viewport" content="width=device-width, initial-scale=1.0">--}}
+{{--    <title>Register</title>--}}
+{{--    <style>--}}
+{{--        body {--}}
+{{--            font-family: Arial, sans-serif;--}}
+{{--            background-color: #f4f4f4;--}}
+{{--            display: flex;--}}
+{{--            justify-content: center;--}}
+{{--            align-items: center;--}}
+{{--            height: 100vh;--}}
+{{--            margin: 0;--}}
+{{--        }--}}
+
+{{--        .register-container {--}}
+{{--            background-color: #ffffff;--}}
+{{--            padding: 20px;--}}
+{{--            border-radius: 8px;--}}
+{{--            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);--}}
+{{--            text-align: center;--}}
+{{--            width: 400px; /* Set a fixed width for the container */--}}
+{{--        }--}}
+
+{{--        img {--}}
+{{--            width: 100px;--}}
+{{--            margin-bottom: 15px;--}}
+{{--        }--}}
+
+{{--        form {--}}
+{{--            display: flex;--}}
+{{--            flex-direction: column;--}}
+{{--            align-items: center; /* Center align form elements */--}}
+{{--        }--}}
+
+{{--        input[type="text"],--}}
+{{--        input[type="password"],--}}
+{{--        button {--}}
+{{--            width: 100%;--}}
+{{--            padding: 10px;--}}
+{{--            margin-bottom: 15px;--}}
+{{--            border: 1px solid #ddd;--}}
+{{--            border-radius: 4px;--}}
+{{--            box-sizing: border-box; /* Ensure padding does not affect width */--}}
+{{--        }--}}
+
+{{--        button {--}}
+{{--            background-color: #28a745; /* Use a different color for registration button */--}}
+{{--            color: #fff;--}}
+{{--            border: none;--}}
+{{--            cursor: pointer;--}}
+{{--        }--}}
+
+{{--        a {--}}
+{{--            text-decoration: none;--}}
+{{--            color: #007bff;--}}
+{{--        }--}}
+
+{{--        a:hover {--}}
+{{--            text-decoration: underline;--}}
+{{--        }--}}
+{{--        .form-select{--}}
+{{--            background-color: #ffffff;--}}
+{{--            padding: 10px;--}}
+{{--            border-radius: 5px;--}}
+{{--            width: 400px;--}}
+{{--            margin-bottom: 10px;--}}
+{{--        }--}}
+{{--        .form-option-list{--}}
+{{--            font-size: 16px;--}}
+{{--            padding: 8px;--}}
+{{--        }--}}
+{{--    </style>--}}
+{{--        @livewireStyles--}}
+{{--        @livewireScripts--}}
+{{--</head>--}}
+{{--<body dir="rtl">--}}
+{{--<div class="register-container">--}}
+{{--    <img src="{{asset('images/register.png')}}" alt="Logo">--}}
+{{--    <h2>Register</h2>--}}
+{{--    @livewire('register-form',['provinces' => $data])--}}
+{{--    <p> لیست تمام ادمین های در سیستم.  <a href="{{route('province_liat')}}">اینجا کلیک کنید </a></p>--}}
+
+{{--</div>--}}
+{{--</body>--}}
+{{--</html>--}}
+
+
+    <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f4f4f4;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            margin: 0;
-        }
-
-        .register-container {
-            background-color: #ffffff;
-            padding: 20px;
-            border-radius: 8px;
-            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-            text-align: center;
-            width: 400px; /* Set a fixed width for the container */
-        }
-
-        img {
-            width: 100px;
-            margin-bottom: 15px;
-        }
-
-        form {
-            display: flex;
-            flex-direction: column;
-            align-items: center; /* Center align form elements */
-        }
-
-        input[type="text"],
-        input[type="password"],
-        button {
-            width: 100%;
-            padding: 10px;
-            margin-bottom: 15px;
-            border: 1px solid #ddd;
-            border-radius: 4px;
-            box-sizing: border-box; /* Ensure padding does not affect width */
-        }
-
-        button {
-            background-color: #28a745; /* Use a different color for registration button */
-            color: #fff;
-            border: none;
-            cursor: pointer;
-        }
-
-        a {
-            text-decoration: none;
-            color: #007bff;
-        }
-
-        a:hover {
-            text-decoration: underline;
-        }
-        .form-select{
-            background-color: #ffffff;
-            padding: 10px;
-            border-radius: 5px;
-            width: 400px;
-            margin-bottom: 10px;
-        }
-        .form-option-list{
-            font-size: 16px;
-            padding: 8px;
-        }
-    </style>
-        @livewireStyles
-        @livewireScripts
+    <title>Profile Page</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
 </head>
-<body dir="rtl">
-<div class="register-container">
-    <img src="{{asset('images/register.png')}}" alt="Logo">
-    <h2>Register</h2>
-    @livewire('register-form',['provinces' => $data])
-    <p> لیست تمام ادمین های در سیستم.  <a href="{{route('province_liat')}}">اینجا کلیک کنید </a></p>
+<body>
 
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Profile</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Services</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<!-- Profile Section -->
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-body text-center">
+                    <div class="position-relative d-inline-block mb-4">
+                        <img src="https://via.placeholder.com/150" alt="Profile Picture" class="rounded-circle" width="150" height="150">
+                        <a href="#" class="position-absolute" style="bottom: 0; right: 0; background-color: rgba(0, 0, 0, 0.5); color: white; border-radius: 50%; padding: 5px;">
+                            <i class="fas fa-edit"></i>
+                        </a>
+                    </div>
+                    <h3>John Doe</h3>
+                    <p class="text-muted">Action Type: Admin</p>
+                    <p class="text-muted">Date: 2024-07-28</p>
+                    <div class="mt-3">
+                        <a href="#" class="btn btn-primary">Edit Profile</a>
+                        <a href="#" class="btn btn-secondary">Settings</a>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Additional Information Section -->
+            <div class="card mt-4">
+                <div class="card-header">
+                    Additional Information
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Bio</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac ante mollis quam tristique convallis.</p>
+                    <h5 class="card-title">Contact Information</h5>
+                    <p class="card-text">Email: johndoe@example.com</p>
+                    <p class="card-text">Phone: (123) 456-7890</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+
+<!-- Footer -->
+<footer class="bg-dark text-white text-center py-3 mt-5">
+    &copy; 2024 Profile Page. All rights reserved.
+</footer>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script>
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip();
+    });
+</script>
 </body>
 </html>
