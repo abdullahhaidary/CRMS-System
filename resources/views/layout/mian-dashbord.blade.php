9adf9b3743d535029f0bf677bc4234d361bdf3ee
abdullahhaidary4262@gmail.com <abdullahhadiary4262@gmail.com>
2024-08-06 17:46:55 +0430

    a comment removed from the page

--- resources/views/layout/mian-dashbord.blade.php
+++ resources/views/layout/mian-dashbord.blade.php
@@ -232,7 +232,6 @@
             </div>
         </div>
     </div>
-    <!-- end modal -->
     <div id="main">
         <header class="mb-3">
             <a href="#" class="burger-btn d-block d-xl-none">
