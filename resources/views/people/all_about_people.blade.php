f886b5fb79821db8d0c31225dd972b978ffd3ce7
abdullahhaidary4262@gmail.com <abdullahhadiary4262@gmail.com>
2024-08-08 11:37:52 +0430

    pdf is working now and also some changes were brought to the people controller

--- resources/views/people/all_about_people.blade.php
+++ resources/views/people/all_about_people.blade.php
@@ -1,20 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>All About Case PDF</title>
+    <link href="https://fonts.googleapis.com/css2?family=Vazirmatn:wght@400;700&display=swap" rel="stylesheet">
     <style>
         @font-face {
-            font-family: 'NotoNaskhArabic';
-            src: url('{{ public_path('fonts/NotoNaskhArabic-Regular.ttf') }}') format('truetype');
+            font-family: 'Vazirmatn', sans-serif;
+            direction:rtl;
             font-weight: normal;
             font-style: normal;
         }
 
         body {
-            font-family: 'NotoNaskhArabic', sans-serif;
-            direction: rtl;
+            font-family: 'Vazirmatn', sans-serif;
             margin: 0;
             padding: 20px;
             background-color: #f8f9fa;
@@ -61,7 +61,7 @@
         .col-md-6 {
             padding-right: 15px;
             padding-left: 15px;
-            box-sizing: border-box; /* Ensure padding is included in the width */
+            box-sizing: border-box;
         }
 
         .col-12 {
@@ -112,114 +112,173 @@
             border: 1px solid #ccc;
         }
     </style>
+    <script src="{{asset('dist/js/jquery .js')}}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/printThis/1.15.0/printThis.min.js"></script>
+
 </head>
 <body>
-<div class="container">
-    <div class="text-center mb-4">
-        <!--  صفحه PDF یک شکایت -->
-        <h1>{{__('Complaint_details')}}</h1>
-    </div>
-    @foreach($peoples as $item)
-        <div class="mb-4">
-            <a href="{{url('/generate-pdf/'.$item->id)}}" class="btn">Download PDF</a>
+    <div class="container">
+        <div class="text-center mb-4">
+            <h1>{{__('Complaint_details')}}</h1>
         </div>
-        <div class="row mb-4">
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="card-header">
-                        <h5 class="card-title">Complaint Information</h5>
-                    </div>
-                    <div class="card-body">
-                        <p><strong>#: </strong>{{$item->id}}</p>
-                        <p><strong>{{__('Name')}}: </strong>{{$item->name . " ". $item->last_name}}</p>
-                        <p><strong>{{__('Father_name')}}: </strong>{{ $item->father_name}}</p>
-                        <p><strong>{{__('Email')}}: </strong>{{ $item->email}}</p>
-                        <p><strong>{{__('Phone_number')}}: </strong>{{ $item->phone}}</p>
-                        <p><strong>{{__('Tazkera_number')}}: </strong>{{ $item->tazkira_number}}</p>
-                        <p><strong>{{__('Main_address')}}: </strong>{{ $item->actual_address}}</p>
-                        <p><strong>{{__('Current_address')}}: </strong>{{ $item->current_address}}</p>
-                        <p><strong>{{__('Case')}}: </strong>{{ $item->crime_case}}</p>
-                        <p><strong>{{__('Crime_subject')}}: </strong>{{ $item->subject_crim}}</p>
-                        <p><strong>{{__('Crime_date')}}: </strong>{{ $item->crim_date}}</p>
-                        <p><strong>{{__('Create_date')}}: </strong> {{$item->created_at}}</p>
-                    </div>
-                </div>
-            </div>
+        <div class="mb-4">
+            <button onclick="downloadDivAsPDF()" class="btn">Download PDF</button>
 
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="card-header">
-                        <h5 class="card-title">Evidence</h5>
+        </div>
+        <div id="content-to-download">
+            @foreach($peoples as $item)
+                <div class="row mb-4">
+                    <div class="col-md-6">
+                        <div class="card">
+                            <div class="card-header">
+                                <h5 class="card-title">Complaint Information</h5>
+                            </div>
+                            <div class="card-body">
+                                <p><strong>#: </strong>{{$item->id}}</p>
+                                <p><strong>{{__('Name')}}: </strong>{{$item->name . " ". $item->last_name}}</p>
+                                <p><strong>{{__('Father_name')}}: </strong>{{ $item->father_name}}</p>
+                                <p><strong>{{__('Email')}}: </strong>{{ $item->email}}</p>
+                                <p><strong>{{__('Phone_number')}}: </strong>{{ $item->phone}}</p>
+                                <p><strong>{{__('Tazkera_number')}}: </strong>{{ $item->tazkira_number}}</p>
+                                <p><strong>{{__('Main_address')}}: </strong>{{ $item->actual_address}}</p>
+                                <p><strong>{{__('Current_address')}}: </strong>{{ $item->current_address}}</p>
+                                <p><strong>{{__('Case')}}: </strong>{{ $item->crime_case}}</p>
+                                <p><strong>{{__('Crime_subject')}}: </strong>{{ $item->subject_crim}}</p>
+                                <p><strong>{{__('Crime_date')}}: </strong>{{ $item->crim_date}}</p>
+                                <p><strong>{{__('Create_date')}}: </strong> {{$item->created_at}}</p>
+                            </div>
+                        </div>
                     </div>
-                    <div class="card-body">
-                        <ul class="list-group">
-                            <li class="list-group-item">Photographs</li>
-                            <li class="list-group-item">Witness Statements</li>
-                            <li class="list-group-item">Video Footage</li>
-                        </ul>
+                    <div class="col-md-6">
+                        <div class="card">
+                            <div class="card-header">
+                                <h5 class="card-title">Evidence</h5>
+                            </div>
+                            <div class="card-body">
+                                <ul class="list-group">
+                                    <li class="list-group-item">Photographs</li>
+                                    <li class="list-group-item">Witness Statements</li>
+                                    <li class="list-group-item">Video Footage</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+            <div class="row mb-4">
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="card-header">
+                            <h5 class="card-title">Suspect Information</h5>
+                        </div>
+                        @foreach($suspects as $suspect)
+                            <div class="card-body">
+                                <p><strong>{{__('Suspect_name')}}:</strong>  {{$suspect->name}}</p>
+                                <p><strong>{{__('Last_name')}}:</strong>  {{$suspect->last_name}}</p>
+                                <p><strong>{{__('Father_name')}}:</strong>  {{$suspect->father_name}}</p>
+                                <p><strong>{{__('Phone_number')}}:</strong>  {{$suspect->phone}}</p>
+                                <p><strong>{{__('Tazkera_number')}}:</strong>  {{$suspect->tazcira_number}}</p>
+                                <p><strong>{{__('Main_address')}}:</strong>  {{$suspect->actual_address}}</p>
+                                <p><strong>{{__('Current_address')}}:</strong>  {{$suspect->current_address}}</p>
+                                <p><strong>{{__('Create_date')}}:</strong>  {{$suspect->created_at}}</p>
+                            </div>
+                            <hr>
+                        @endforeach
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="card-header">
+                            <h5 class="card-title">Case Details</h5>
+                        </div>
+                        @foreach($cases as $case)
+                            <div class="card-body">
+                                <p><strong>{{__('Case_number')}}:</strong> {{$case->case_number}}</p>
+                                <p><strong>{{__('Case_status')}}:</strong> {{$case->status}}</p>
+                                <p><strong>{{__('Case_type')}}:</strong> {{$case->crime_location}}</p>
+                                <p><strong>{{__('Start_date')}}:</strong> {{$case->start_date}}</p>
+                                <p><strong>{{__('End_date')}}:</strong> {{$case->end_date}}</p>
+                                <p><strong>{{__('Description')}}:</strong> {{$case->description}}</p>
+                                <p><strong>{{__('Create-date')}}:</strong> {{$case->created_at}}</p>
+                            </div>
+                        @endforeach
                     </div>
                 </div>
             </div>
+            <div class="row mb-4">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h5 class="card-title">Description</h5>
+                        </div>
+                        @foreach($info as $items)
+                            <div class="card-body">
+                                <p>{{$items->description}}.</p>
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="content-to-download2" dir="rtl">
+            @foreach($peoples as $item)
+                <div class="row mb-4">
+                    <div class="col-md-6">
+                        <div class="card">
+                            <div class="card-header">
+                                <h5 class="card-title">معلومات شکایت کننده</h5>
+                            </div>
+                            <div class="card-body">
+                                <p><strong>#: </strong>{{$item->id}}</p>
+                                <p><strong>{{__('Name')}}: </strong>{{$item->name . " ". $item->last_name}}</p>
+                                <p><strong>{{__('Father_name')}}: </strong>{{ $item->father_name}}</p>
+                                <p><strong>{{__('Email')}}: </strong>{{ $item->email}}</p>
+                                <p><strong>{{__('Phone_number')}}: </strong>{{ $item->phone}}</p>
+                                <p><strong>{{__('Tazkera_number')}}: </strong>{{ $item->tazkira_number}}</p>
+                                <p><strong>{{__('Main_address')}}: </strong>{{ $item->actual_address}}</p>
+                                <p><strong>{{__('Current_address')}}: </strong>{{ $item->current_address}}</p>
+                                <p><strong>{{__('Case')}}: </strong>{{ $item->crime_case}}</p>
+                                <p><strong>{{__('Crime_subject')}}: </strong>{{ $item->subject_crim}}</p>
+                                <p><strong>{{__('Crime_date')}}: </strong>{{ $item->crim_date}}</p>
+                                <p><strong>{{__('Create_date')}}: </strong> {{$item->created_at}}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="card">
+                            <div class="card-header">
+                                <h5 class="card-title">شواهد</h5>
+                            </div>
+                            <div class="card-body">
+                                <ul class="list-group">
+                                    <li class="list-group-item">Photographs</li>
+                                    <li class="list-group-item">Witness Statements</li>
+                                    <li class="list-group-item">Video Footage</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             @endforeach
         </div>
+    </div>
+    <script>
+function downloadDivAsPDF() {
+    $('#content-to-download2').printThis({
+        importCSS: true,            // import parent page css
+        importStyle: true,          // import style tags
+        loadCSS: "path/to/your/custom.css", // path to additional css file - use an array [] for multiple
+        pageTitle: "Complaint Details", // add title to print page
+        header: null,               // prefix to html
+        footer: null,               // postfix to html
+        base: false,                // preserve the BASE tag or accept a string for the URL
+        formValues: true,           // preserve input/form values
+        canvas: true,               // copy canvas content
+        doctypeString: '<!DOCTYPE html>', // html doctype
+    });
+}
 
-        <div class="row mb-4">
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="card-header">
-                        <h5 class="card-title">Suspect Information</h5>
-                    </div>
-                    @foreach($suspects as $suspect)
-                        <div class="card-body">
-                            <p><strong>{{__('Suspect_name')}}:</strong>  {{$suspect->name}}</p>
-                            <p><strong>{{__('Last_name')}}:</strong>  {{$suspect->last_name}}</p>
-                            <p><strong>{{__('Father_name')}}:</strong>  {{$suspect->father_name}}</p>
-                            <p><strong>{{__('Phone_number')}}:</strong>  {{$suspect->phone}}</p>
-                            <p><strong>{{__('Tazkera_number')}}:</strong>  {{$suspect->tazcira_number}}</p>
-                            <p><strong>{{__('Main_address')}}:</strong>  {{$suspect->actual_address}}</p>
-                            <p><strong>{{__('Current_address')}}:</strong>  {{$suspect->current_address}}</p>
-                            <p><strong>{{__('Create_date')}}:</strong>  {{$suspect->created_at}}</p>
-                        </div>
-                        <hr>
-                    @endforeach
-                </div>
-            </div>
 
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="card-header">
-                        <h5 class="card-title">Case Details</h5>
-                    </div>
-                    @foreach($cases as $case)
-                        <div class="card-body">
-                            <p><strong>{{__('Case_number')}}:</strong> {{$case->case_number}}</p>
-                            <p><strong>{{__('Case_status')}}:</strong> {{$case->status}}</p>
-                            <p><strong>{{__('Case_type')}}:</strong> {{$case->crime_location}}</p>
-                            <p><strong>{{__('Start_date')}}:</strong> {{$case->start_date}}</p>
-                            <p><strong>{{__('End_date')}}:</strong> {{$case->end_date}}</p>
-                            <p><strong>{{__('Description')}}:</strong> {{$case->description}}</p>
-                            <p><strong>{{__('Create-date')}}:</strong> {{$case->created_at}}</p>
-                        </div>
-                    @endforeach
-                </div>
-            </div>
-        </div>
-
-        <div class="row mb-4">
-            <div class="col-12">
-                <div class="card">
-                    <div class="card-header">
-                        <h5 class="card-title">Description</h5>
-                    </div>
-                    @foreach($info as $items)
-                        <div class="card-body">
-                            <p>{{$items->description}}.</p>
-                        </div>
-                    @endforeach
-                </div>
-            </div>
-        </div>
-</div>
+    </script>
 </body>
 </html>
-
