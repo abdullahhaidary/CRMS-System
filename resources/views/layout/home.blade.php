208bb8b35323120fbf959e8f719d64d9b633572c
abdullahhaidary4262@gmail.com <abdullahhadiary4262@gmail.com>
2024-08-12 18:08:21 +0430

    dashboard is now more dynamic than ever before

--- resources/views/layout/home.blade.php
+++ resources/views/layout/home.blade.php
@@ -65,8 +65,8 @@ if (!function_exists('trim_to_words')) {
                                         </div>
                                     </div>
                                     <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                        <h6 class="text-muted font-semibold">زندانی ها</h6>
-                                        <h6 class="font-extrabold mb-0">80.000</h6>
+                                        <h6 class="text-muted font-semibold">{{__('total_suspect')}}</h6>
+                                        <h6 class="font-extrabold mb-0">{{$total_suspect}}</h6>
                                     </div>
                                 </div>
                             </div>
